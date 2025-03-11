a25848e52a54240180457dfcf488e1ed5aa79f75
secopsninjaaws <lucas@secopsninjaaws.com.br>
2025-03-11 09:12:40 -0300

    Update private_subnets.tf

--- private_subnets.tf
+++ private_subnets.tf
@@ -44,13 +44,15 @@ resource "aws_nat_gateway" "main" {
 
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
+    route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main.id
+} 
+  tags = {
+    Name = format("%s-private", var.project_name)
+  }
 }
 
-resource "aws_route" "private" {
-  route_table_id         = aws_route_table.private.id
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.main.id
-}
 
 resource "aws_route_table_association" "main_private_1a" {
   subnet_id      = aws_subnet.private_aws_subnet_1a.id
